dad2181b155e6b7bfe550f3669feb5acf70f5c43
Gaston Donikian <gastondonikian@gmail.com>
2025-04-11 17:12:20 -0300

    Adding problem set 4 and moving wrongly placed file to problem set 5

--- Guia4/guia4.hs
+++ Guia4/guia4.hs
@@ -1,45 +1,70 @@
-data Gusto = Chocolate | DulceDeLeche | Frutilla | Sambayon
-data Helado = Vasito Gusto | Cucurucho Gusto Gusto | Pote Gusto Gusto Gusto
+-- Ejercicio 1.
 
-chocoHelate consH = consH Chocolate
+-- a. Funcion parcial, div x 0 da bottom
+udiv (x, y) = div x y
+-- b. Funcion parcial, udivE (x, 0) da bottom
+udivE (x, 0) = error "No puedo dividir por 0"
+udivE (x, y) = div x y
+-- c. Funcion parcial, udivH (x,0) da bottom
+udivH = uncurry div
+-- d. Funcion total
+succ x = x + 1
+-- e. Funcion total
+succH = suma 1
+-- f. Funcion total
+porLaMitad = flip div 2
+-- g. Funcion parcial, conDieresis 'L' rompe
+conDieresis 'u' = 'U'
+-- h. Funcion parcial, nunca termina en 'L'
+conDieresisB 'u' = 'U'
+conDieresisB c = conDieresisB c
+-- i. Funcion parcial, faltan casos
+conTildePM 'a' = 'a'
+conTildePM 'e' = 'e'
+conTildePM 'i' = 'i'
+conTildePM 'o' = 'o'
+conTildePM 'u' = 'u'
+-- j. Funcion parcial, bottom
+conTildeE c = if esVocal c
+                 then conTildePM c
+              else error "El valor recibido no es vocal"
+-- k. Funcion total
+conTilde c = if esVocal c && esMinuscula c
+                then conTildePM c
+             else c
 
-// Ejercicio 1
-// Vasito :: Gusto -> Helado
-// Chocolate :: Gusto
-// Cucurucho :: Gusto -> Gusto -> Helado
-// Sambayon :: Gusto
-// Pote :: Gusto -> Gusto -> Gusto -> Helado
-// chocoHelate :: (Gusto -> b) -> b
-// chocoHelate Vasito :: Helado
-// chocoHelate Cucurucho :: Gusto -> Helado
-// chocoHelate (Cucurucho Sambayon) :: Helado
-// chocoHelate (Vasito DulceDeLeche) :: NO TIPA
-// chocoHelate Pote :: Gusto -> Gusto -> Helado
-// chocoHelate (chocoHelate (Pote Frutilla)) :: Helado
+-- Ejercicio 2
+-- udiv y udivH
+-- succ y succH
+-- conDieresis y conDieresisB
+-- conTildePM y conTildeE
 
-data DigBin = O | I
-dbAsInt :: DigBin -> Int
-dbAsInt O = 0
-dbAsInt I = 1
+-- Ejercicio 3.
+twice = \f -> \x -> f (f x)
+-- a. 2 veces
+-- twice doble
+-- (\f -> \x -> f (f x)) doble
+-- (\x -> doble (doble x)
 
-dbAsBool :: DigBin -> Bool
-dbAsBool O = False
-dbAsBool I = True
+-- b. 8 veces
+-- twice doble 2
+-- (\f -> \x -> f (f x)) doble 2
+-- (\x -> doble (doble x)) 2
+-- doble (doble 2)
+-- doble (2 * 2)
+-- doble 4
+-- 4 * 2
+-- 8
 
-dbOfBool :: Bool -> DigBin
-dbOfBool True = I
-dbOfBool False = O
+-- c. 1 vez
+-- twice
+-- (\f -> \x -> f (f x))
 
-negDB :: DigBin -> DigBin
-negDB I = O
-negDB O = I
+-- Ejercicio 4.
+-- Ejercicio 5.
 
-
-
-
-
-
-
-
-
+-- Ejercicio 6.
+-- a. No hay
+-- b. id int = error "..."
+-- c. f x = error "..." 
 
